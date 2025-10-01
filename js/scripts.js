3f5420291976b8bb1cfa7ec57c55fed8505da574
Niamh Fitzsimon <niamhfitzsimon7@gmail.com>
2025-10-01 14:44:54 -0700

    feat: Build out About Me section
    Fetches data from aboutMeData.json
    
    Builds and adds elements for that data

---
+++ js/scripts.js
@@ -0,0 +1,38 @@
+const rebuildAboutMe = async () => {
+  fetch('./data/aboutMeData.json')
+    .then(response => {
+      // Check if the response was successful (status code 200-299)
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      return response.json();
+    })
+    .then(data => {
+      fillAboutMe(data);
+    })
+    .catch(error => {
+      // Handle any errors that occurred during the fetch operation
+      console.error('Error fetching JSON data:', error);
+    })
+}
+
+function fillAboutMe(data) {
+  const documentFragment = document.createDocumentFragment();
+
+  const aboutDiv = document.querySelector('#aboutMe');
+  const aboutParagraph = document.createElement('p');
+  aboutParagraph.textContent = data.aboutMe;
+  const headshotContainer = document.createElement('div');
+  headshotContainer.classList.add('headshotContainer');
+  const headshot = document.createElement('img');
+  headshot.src = data.headshot;
+
+  headshotContainer.append(headshot);
+
+  documentFragment.append(aboutParagraph);
+  documentFragment.append(headshotContainer);
+
+  aboutDiv.append(documentFragment);
+}
+
+rebuildAboutMe();
