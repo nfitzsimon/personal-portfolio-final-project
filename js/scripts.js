550dc191003b70d752c8383702097eb9b773360e
Niamh Fitzsimon <niamhfitzsimon7@gmail.com>
2025-10-01 15:18:38 -0700

    feat: Build out project list

--- js/scripts.js
+++ js/scripts.js
@@ -35,4 +35,45 @@ function fillAboutMe(data) {
   aboutDiv.append(documentFragment);
 }
 
+const rebuildProjects = async () => {
+  fetch('./data/projectsData.json')
+    .then(response => {
+      // Check if the response was successful (status code 200-299)
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      return response.json();
+    })
+    .then(data => {
+      fillProjects(data);
+    })
+    .catch(error => {
+      // Handle any errors that occurred during the fetch operation
+      console.error('Error fetching JSON data:', error);
+    })
+}
+
+function fillProjects(data) {
+  const projectList = document.querySelector('#projectList');
+  data.forEach((element, index, array) => {
+    const project = document.createElement('div');
+    project.id = element.project_id;
+    project.classList.add('projectCard');
+    const backgroundImageUrl = `url(${element.card_image})`
+    project.style.backgroundImage = backgroundImageUrl;
+    project.style.backgroundSize = "cover";
+
+    const projectHeader = document.createElement('h4');
+    projectHeader.textContent = element.project_name;
+    project.append(projectHeader);
+
+    const projectText = document.createElement('p');
+    projectText.textContent = element.short_description;
+    project.append(projectText);
+
+    projectList.append(project);
+  });
+}
+
 rebuildAboutMe();
+rebuildProjects();
